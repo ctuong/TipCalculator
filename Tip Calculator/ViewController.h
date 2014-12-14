e5b30e22ef88210cbb1f2ebfecc1b988e2fbf2e1
Calvin Tuong <ctuong@box.com>
2014-12-14 13:38:01 -0800

    Initial Commit

---
+++ Tip Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tip Calculator
+//
+//  Created by Calvin Tuong on 12/14/14.
+//  Copyright (c) 2014 Calvin Tuong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
